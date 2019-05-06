165cc6a3ff5fd81575101b058ca07974708a49d0
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2019-05-06 23:31:27 +0700

    Add files via upload

---
+++ Lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
+
+
+def home(request):
+    return render(request, 'templates/static_handler.html')
+
+
+def hello(request):
+    return HttpResponse(u'Hello, World!')
